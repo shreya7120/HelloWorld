9069eefaa146fbeebbebeed11745aeca509381fa
user <user@DESKTOP-TR4LISM>
2024-04-06 21:58:27 +0530

    Initial Commit

---
+++ JAVAPROGRAMS/src/test_pack/Demo1.java
@@ -0,0 +1,13 @@
+package test_pack;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Running Demo 1");	
+		// TODO Auto-generated method stub
+		System.out.println("Running Demo 1");	
+	}
+
+}
+ 
