8f6a9208f9c6c484368e259f6e5dd6b0173b8cf7
user <user@DESKTOP-TR4LISM>
2024-04-09 12:57:30 +0530

    next

---
+++ JAVAPROGRAMS/src/simple_insta_programs/ProUnicode1.java
@@ -0,0 +1,11 @@
+package simple_insta_programs;
+
+public class ProUnicode1 {
+
+	public static void main(String[] args) {
+		
+		//\u000d System.out.println("hello"); // this is giving output hello cause \u000d is a new line unicode character
+
+	}
+
+}
